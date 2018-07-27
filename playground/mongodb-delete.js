af373afdc6e33fbbea14801089556355d2690070
Rohan Kalgutkar <hello@rohankalgutkar.in>
2018-07-27 10:37:34 +0530

    Refactoring, mongoose changes

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,23 @@
+const {
+  MongoClient,
+  ObjectID
+} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {
+  useNewUrlParser: true
+}, (err, client) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  const db = client.db('TodoApp');
+
+  // delete many
+
+  // deleteOne
+
+  //findOneAndDelete
+
+  // client.close();
+});

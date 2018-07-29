efb15bb85bf76f023a1e88e56073313cbc420743
Rohan Kalgutkar <hello@rohankalgutkar.in>
2018-07-29 13:56:33 +0530

    Patch

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var {
+  mongoose
+} = require('./db/mongoose');
+var {
+  Todo
+} = require('./models/todo');
+var {
+  User
+} = require('./models/user');
+var {
+  ObjectID
+} = require('mongodb');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -22,9 +32,44 @@ app.post('/todos', (req, res) => {
 });
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos})
+    res.send({
+      todos
+    })
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  // validating the ID
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+console.log(body);
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, {
+    new: true
+  }).then((todo) => {
+    if (!todo) {
+      res.status(404).send();
+    }
+    res.send({
+      todo
+    });
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
@@ -32,4 +77,6 @@ app.listen(port, () => {
   console.log(`Started on Port ${port}`);
 });
 
-module.exports = {app};
+module.exports = {
+  app
+};
